95c00389d34eff67654945cb3ceeabf4aa08c18d
tanishq931 <sharma.tanishq931@gmail.com>
2023-09-13 19:14:04 +0530

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:movie_app/Screens/HomeScreen.dart';
-import 'package:movie_app/Splash.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main(){
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 class MyApp extends StatefulWidget {
